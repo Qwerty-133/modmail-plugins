5dde7411fdef0f52693e51ea25394b61f74fb3a0
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2021-07-10 05:28:32 +0530

    add close_silently command

--- close/close.py
+++ close/close.py
@@ -10,9 +10,11 @@ DEFAULT_MESSAGE = "Feel free to open a new thread if you need anything else."
 class UserFriendlyTimeOnly(time.UserFriendlyTime):
     async def convert(self, ctx, argument):
         converted = await super().convert(ctx, argument)
-        if converted.arg:
+        if not converted.arg:
+            converted.arg = DEFAULT_MESSAGE
+        elif converted.arg not in ('silently', 'silently'):
             raise commands.BadArgument('A message cannot be supplied.')
-        converted.arg = DEFAULT_MESSAGE
+
         return converted
 
 
@@ -21,14 +23,20 @@ class CloseMessage(commands.Cog):
         self.bot = bot
         self.close_command = self.bot.get_command('close')
 
-    @commands.command(name='closemessage', aliases=['cm'], usage="[after]")
+    @commands.command(name='c', usage="[after]")
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @checks.thread_only()
-    async def close_message(self, ctx, *, after: UserFriendlyTimeOnly = None):
+    async def close(self, ctx, *, after='15m'):
         """Close the current thread with the default message."""
-        if not after:
-            after = await UserFriendlyTimeOnly().convert(ctx, 'in 15m')
+        after = await UserFriendlyTimeOnly().convert(ctx, after)
+        return await self.close_command(ctx, after=after)
 
+    @commands.command(name='cs', usage="[after]")
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    @checks.thread_only()
+    async def close_silently(self, ctx, *, after='15m'):
+        """Close the current thread silently."""
+        after = await UserFriendlyTimeOnly().convert(ctx, f'{after} silently')
         return await self.close_command(ctx, after=after)
 
 
