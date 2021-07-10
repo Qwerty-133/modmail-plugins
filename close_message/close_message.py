903235450d391ba6393006a09eb2e1a70112beb9
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2021-07-10 10:40:24 +0530

    Default duration of 15 minutes.

--- close_message/close_message.py
+++ close_message/close_message.py
@@ -19,13 +19,15 @@ class UserFriendlyTimeOnly(time.UserFriendlyTime):
 class CloseMessage(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.close_command = self.bot.get_command('close')
 
     @commands.command(name='closemessage', aliases=['cm'], usage="[after]")
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @checks.thread_only()
-    async def close_message(self, ctx, *, after: UserFriendlyTimeOnly = None):
+    async def close_message(self, ctx, *, after='15m'):
         """Close the current thread with the default message."""
-        return await self.bot.get_command('close')(ctx, after=after)
+        after = await UserFriendlyTimeOnly().convert(ctx, after)
+        return await self.close_command(ctx, after=after)
 
 
 def setup(bot):
