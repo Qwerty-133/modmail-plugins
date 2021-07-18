9c90d3aee95e1c4af61481b3c199d4c574cede66
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2021-07-18 16:31:55 +0530

    Make the help text display the closing message

--- close_message/close_message.py
+++ close_message/close_message.py
@@ -4,7 +4,7 @@ from core import checks
 from core import time
 from core.models import PermissionLevel
 
-DEFAULT_MESSAGE = "Feel free to open a new thread if you need anything else."
+CLOSING_MESSAGE = "Feel free to open a new thread if you need anything else."
 
 
 class UserFriendlyTimeOnly(time.UserFriendlyTime):
@@ -14,7 +14,7 @@ class UserFriendlyTimeOnly(time.UserFriendlyTime):
             raise commands.BadArgument(
                 f'`{argument}` isn\'t a valid duration string.'
             )
-        converted.arg = DEFAULT_MESSAGE
+        converted.arg = CLOSING_MESSAGE
         return converted
 
 
@@ -23,11 +23,13 @@ class CloseMessage(commands.Cog):
         self.bot = bot
         self.close_command = self.bot.get_command('close')
 
-    @commands.command(name='closemessage', aliases=['cm'], usage="[after]")
+    @commands.command(name='closemessage', aliases=['cm'],
+                      usage="[after]",
+                      help='Close the current thread with the message '
+                           f'`{CLOSING_MESSAGE}`')
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @checks.thread_only()
     async def close_message(self, ctx, *, after='15m'):
-        """Close the current thread with the default message."""
         after = await UserFriendlyTimeOnly().convert(ctx, after)
         return await self.close_command(ctx, after=after)
 
