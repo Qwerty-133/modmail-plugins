26d9d80822b3aa01dfc803452026157847183ffb
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2021-07-29 13:34:07 +0530

    Patch the instance, not the type

--- case_insensitive_snippets/case_insensitive_snippets.py
+++ case_insensitive_snippets/case_insensitive_snippets.py
@@ -1,14 +1,14 @@
 def setup(bot):
     old_process_commands = bot.process_commands
 
-    async def process_commands(self, message):
-        if message.content.startswith(self.prefix):
-            cmd = message.content[len(self.prefix):].strip().lower()
+    async def process_commands(message):
+        if message.content.startswith(bot.prefix):
+            cmd = message.content[len(bot.prefix):].strip().lower()
 
             # Convert message contents to lowercase if a snippet exists
-            if cmd in self.snippets:
-                message.content = self.prefix + cmd
+            if cmd in bot.snippets:
+                message.content = bot.prefix + cmd
 
         await old_process_commands(message)
 
-    type(bot).process_commands = process_commands
+    bot.process_commands = process_commands
